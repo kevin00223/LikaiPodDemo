e3885e490e1dc88782b79670fe45d84caa5d5bc2
李凯 <likai.new@gmail.com>
2018-12-18 11:40:04 +0800

    init project

---
+++ LikaiPod/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LikaiPod
+//
+//  Created by 李凯 on 2018/12/18.
+//  Copyright © 2018年 李凯. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ LikaiPodTests/LikaiPodTests.m
@@ -0,0 +1,37 @@
+//
+//  LikaiPodTests.m
+//  LikaiPodTests
+//
+//  Created by 李凯 on 2018/12/18.
+//  Copyright © 2018年 李凯. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LikaiPodTests : XCTestCase
+
+@end
+
+@implementation LikaiPodTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
